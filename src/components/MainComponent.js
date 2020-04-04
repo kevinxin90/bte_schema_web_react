5232d73103a713d75b7aeffd37e072ea1ea1fe84
jiwen xin <kevinxin@scripps.edu>
2020-04-03 17:16:26 -0700

    update main component

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,6 +4,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import IDResolver from './IDResolverComponent';
 import Explain from "./ExplainComponent";
+import Predict from "./PredictComponent";
 import About from './AboutComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
@@ -56,7 +57,7 @@ class Main extends Component {
         <Header />
         <Switch>
           <Route path="/explorer_kgs/home" component={HomePage} />
-          <Route exact path="/explorer_kgs/idresolver" component={IDResolver} />
+          <Route exact path="/explorer_kgs/predict" component={() => <Predict options={this.props.options} />} />
           <Route exact path="/explorer_kgs/explain" component={() => <Explain options={this.props.options} />} />
           <Redirect to="/explorer_kgs/home" />
         </Switch>
