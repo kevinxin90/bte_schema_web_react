92068a1f659b21d88bceff5b972fd53c3f659cd1
jiwen xin <kevinxin@scripps.edu>
2020-04-09 09:03:07 -0700

    change dimmer background and add text

--- src/components/DimerComponent.js
+++ src/components/DimerComponent.js
@@ -3,13 +3,12 @@ import { Dimmer, Loader, Image, Segment } from 'semantic-ui-react'
 
 const ReactLoader = () => (
   <Segment>
-    <Dimmer active>
+    <Dimmer active inverted>
       <Loader />
     </Dimmer>
-
-    <Image src='https://react.semantic-ui.com/images/wireframe/short-paragraph.png' />
-    <Image src='https://react.semantic-ui.com/images/wireframe/short-paragraph.png' />
-    <Image src='https://react.semantic-ui.com/images/wireframe/short-paragraph.png' />
+    <h2>The result might take a couple minutes to show up. </h2>
+    <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+    <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
   </Segment>
 )
 
