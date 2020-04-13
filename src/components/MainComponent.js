ad8b146838a7b0c037bb4c75f92e248c0f9f3ad9
jiwen xin <kevinxin@scripps.edu>
2020-04-13 11:38:33 -0700

    remove unused modules

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,14 +2,12 @@ import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
-import About from './AboutComponent';
 import Explain from "./ExplainComponent";
 import Predict from "./PredictComponent";
 import { Route, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { postComment, fetchDishes, fetchComments, fetchPromos } from '../redux/ActionCreators';
 import { actions } from 'react-redux-form';
-import _ from 'lodash';
 
 
 const mapStateToProps = state => {
@@ -50,7 +48,6 @@ class Main extends Component {
       <div>
           <Header />
           <Route exact path='/' component={Home}></Route>
-          <Route exact path='/about' component={About}></Route>
           <Route exact path='/predict' component={Predict}></Route>
           <Route exact path='/explain' component={Explain}></Route>
           <Footer />
