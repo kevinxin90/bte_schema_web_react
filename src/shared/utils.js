8a97cf362f36aa51f4413cf6c10acd9bc42e6e76
jiwen xin <kevinxin@scripps.edu>
2020-07-02 09:02:21 -0700

    update query url

--- src/shared/utils.js
+++ src/shared/utils.js
@@ -1,3 +1,8 @@
+import MetaKG from "@biothings-explorer/smartapi-kg";
+
+const meta_kg = new MetaKG();
+meta_kg.constructMetaKGSync();
+
 const posOrNeg = (num) => {
     if (num === 0) {
         return 0
@@ -8,9 +13,9 @@ const posOrNeg = (num) => {
     }
 }
 
-export const recordsToD3Graph = (records) => {
+const recordsToD3Graph = (records) => {
     records = Array.from(records);
-    let graph = {nodes: [{id: 'kevin'}], links: []};
+    let graph = { nodes: [{ id: 'kevin' }], links: [] };
     if (Array.isArray(records) && records.length) {
         graph['nodes'] = [
             {
@@ -23,29 +28,33 @@ export const recordsToD3Graph = (records) => {
                 id: records[0].split('||')[7],
                 color: 'blue',
                 x: 700,
-                y:200
+                y: 200
             }
         ]
     };
     for (let i = 0; i < records.length; i++) {
         if (i < 10) {
             let rec = records[i].split('||')
-            graph['links'].push({'source': rec[0],
-                                 'target': rec[3],
-                                 'label': rec[1]})
-            graph['links'].push({'source': rec[3],
-                                 'target': rec[7],
-                                 'label': rec[5]})
-            graph['nodes'].push({id: rec[3], color: 'red', x: 360, y: 200 + posOrNeg(i) * Math.ceil(i/2) * 30})
+            graph['links'].push({
+                'source': rec[0],
+                'target': rec[3],
+                'label': rec[1]
+            })
+            graph['links'].push({
+                'source': rec[3],
+                'target': rec[7],
+                'label': rec[5]
+            })
+            graph['nodes'].push({ id: rec[3], color: 'red', x: 360, y: 200 + posOrNeg(i) * Math.ceil(i / 2) * 30 })
         }
     }
     return graph
 }
 
-export const recordsToTreeGraph = (records) => {
+const recordsToTreeGraph = (records) => {
     records = Array.from(records);
     let tree_dict = {};
-    let tree = {children: []}
+    let tree = { children: [] }
     if (Array.isArray(records) && records.length) {
         tree['name'] = records[0].split('||')[0];
     };
@@ -58,29 +67,46 @@ export const recordsToTreeGraph = (records) => {
         }
     };
     for (const prop in tree_dict) {
-        let rec = {name: prop, pathProps: prop, children: []};
-        rec['children'] = Array.from(tree_dict[prop]).map(item=>({name: item, children: []}));
+        let rec = { name: prop, pathProps: prop, children: [] };
+        rec['children'] = Array.from(tree_dict[prop]).map(item => ({ name: item, children: [] }));
         tree['children'].push(rec);
     }
     return tree
 }
 
-export const getIntermediateNodes = (metaPaths) => {
+const getIntermediateNodes = (metaPaths) => {
     return [...metaPaths].map(x => x.split('-').slice(1)[0])
 }
 
-export async function findMetaPath(input_type, output_type) {
+const findMetaPath = async (input_type, output_type) => {
     try {
-        let response = await fetch('https://geneanalysis.ncats.io/explorer_api/v1/find_metapath?input_cls=' + input_type + '&output_cls=' + output_type);
-        response = await response.json();
-        return response['edges'];
-    } catch(err) {
+        // let response = await fetch('http://localhost:8856/explorer_api/v1/find_metapath?input_cls=' + input_type + '&output_cls=' + output_type);
+        // response = await response.json();
+        // return response['edges'];
+        let res1 = new Set(meta_kg.filter({ input_type: input_type }).map(rec => rec.association.output_type));
+        if (res1.size === 0) {
+            return [];
+        }
+
+        let res2 = [];
+        res1.forEach(intermediate => {
+            let tmp = meta_kg.filter({ input_type: intermediate, output_type: output_type });
+            if (tmp.length > 0) {
+                res2.push(input_type + '-' + intermediate + '-' + output_type);
+            }
+        });
+        if (res2.length === 0) {
+            return [];
+        }
+        return res2
+    } catch (err) {
+        console.log(err)
         return [];
     }
 }
 
-export async function fetchQueryResult(input, output, intermediate) {
-    let url = new URL('https:/geneanalysis.ncats.io/explorer_api/v1/connect');
+const fetchQueryResult = async (input, output, intermediate) => {
+    let url = new URL('https://geneanalysis.ncats.io/explorer_api/v1/connect');
     let params = {
         input_obj: JSON.stringify(input),
         output_obj: JSON.stringify(output),
@@ -90,12 +116,15 @@ export async function fetchQueryResult(input, output, intermediate) {
     try {
         let response = await fetch(url);
         if (!response.ok) {
-            return {'data': [], 'log': []}
+            return { 'data': [], 'log': [] }
         };
         response = await response.json();
         return response
-    } catch(err) {
-        return {'data': [], 'log': []}
+    } catch (err) {
+        return { 'data': [], 'log': [] }
     }
 }
 
+//export { findMetaPath, fetchQueryResult, recordsToTreeGraph, recordsToD3Graph, getIntermediateNodes, recordsToTreeGraph };
+
+export { getIntermediateNodes, fetchQueryResult, findMetaPath, recordsToD3Graph, recordsToTreeGraph }
