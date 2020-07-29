000dbaea43812760d35843c3ac6418340a9ef422
jiwen xin <kevinxin@scripps.edu>
2020-07-29 09:58:36 -0700

    update build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/explorer/precache-manifest.63d9cd8467178a4504cac10046586df9.js"
+  "/explorer/precache-manifest.57003135669dde65f85e3e87b5e7f5bf.js"
 );
 
 self.addEventListener('message', (event) => {
