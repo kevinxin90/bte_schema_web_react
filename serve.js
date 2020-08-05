68c4770a961d6bfc9902a64c729cb159e133ec17
jiwen xin <kevinxin@scripps.edu>
2020-08-04 20:53:40 -0700

    change index.js to serve.js

--- serve.js
+++ serve.js
@@ -2,7 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use('/explorer', express.static(path.join(__dirname, '/build')));
-app.get('/explorer', function(req, res) {
+app.get('/explorer', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.get('/', function (req, res) {
+  res.redirect("/explorer")
 });
 app.listen(8853);
